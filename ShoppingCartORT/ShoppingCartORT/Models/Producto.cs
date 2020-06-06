c238074539f5a1c3ec0cbcdd5b5b1222f909a01a
juliangrilli <julianm.grilli@gmail.com>
2020-06-06 13:29:16 -0300

    adding producto

---
+++ ShoppingCartORT/ShoppingCartORT/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ShoppingCartORT.Models
+{
+    public class Producto
+    {
+        public int productoID { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public double precio { get; set; }
+    }
+}
