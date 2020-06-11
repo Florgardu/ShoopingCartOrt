57474d12d31459c5bd738dadfe94875c5f1c5014
Florgardu <garduno.florencia@gmail.com>
2020-06-11 11:32:15 -0300

    Inicialicé la tabla productos con algunos de ej Agregue validaciones para form de registrarse

--- ShoppingCartORT/ShoppingCartORT/Data/DbInitializer.cs
+++ ShoppingCartORT/ShoppingCartORT/Data/DbInitializer.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Linq;
-
+using ShoppingCartORT.Models;
 
 namespace ShoppingCartORT.Data
 {
@@ -9,7 +9,26 @@ namespace ShoppingCartORT.Data
         public static void Initialize(ShoppingCartORTContext context)
         {
             context.Database.EnsureCreated();
+
+            if (context.Productos.Any())
+            {
+                return;
+            }
+            var productos = new Producto[]
+            {
+                new Producto { nombre = "Leche", descripcion = "leche de almendras", precio = 150 },
+                new Producto { nombre = "Huevos", descripcion = "huevos blancos", precio = 50 },
+                new Producto { nombre = "Chocolate", descripcion = "70% cacao", precio = 100 },
+            };
+            foreach (Producto s in productos)
+            {
+                context.Productos.Add(s);
+            }
+            var cantidadInsertada = context.SaveChanges();
+
+
         }
-}
+    }
 
 }
+
--- ShoppingCartORT/ShoppingCartORT/Models/Usuario.cs
+++ ShoppingCartORT/ShoppingCartORT/Models/Usuario.cs
@@ -1,15 +1,35 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
 namespace ShoppingCartORT.Models
 {
     public class Usuario
     {
         public int usuarioID { get; set; }
+
+        [Required(ErrorMessage = "Un nombre es requerido")]
+        [StringLength(20)]
         public string nombre { get; set; }
+
+        [Required(ErrorMessage = "Un apellido es requerido")]
+        [StringLength(20)]
         public string apellido { get; set; }
+
         public int dni { get; set; }
+
+
+        [Required(ErrorMessage = "Password es requerida")]
+        [StringLength(15, ErrorMessage = "La contraseña debe tener como minimo 5 caracteres", MinimumLength = 5)]
         public string password { get; set; }
+
+        [Required(ErrorMessage = "Email es requerido")]
+        [StringLength(30, ErrorMessage = "Debe ingresar un mail entre 5 y 30 caracteres", MinimumLength = 5)]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Ingrese un imail valido")]
         public string mail { get; set; }
 
 
     }
 }
+
